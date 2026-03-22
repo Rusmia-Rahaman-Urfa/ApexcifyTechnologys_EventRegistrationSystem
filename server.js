9db38d8473063a6db84ce8b57eab71116421d652
rusmiarahaman77461@gmail.com <rusmiarahaman77461@gmail.com>
2026-03-22 18:32:17 +0600

    feat: add backend logic to get all events

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => console.log("🚀 Task 2: Cloud Database Connected"))
+    .catch(err => console.log(err));
+
+// MODELS
+const Event = mongoose.model('Event', new mongoose.Schema({
+    title: String,
+    date: String,
+    description: String,
+    category: String // Added for better UI filtering
+}));
+
+const Registration = mongoose.model('Registration', new mongoose.Schema({
+    eventId: { type: mongoose.Schema.Types.ObjectId, ref: 'Event' },
+    userName: String,
+    userEmail: String,
+    status: { type: String, default: 'Confirmed' }
+}));
+
+// API Endpoints
+// Get All Events
+app.get('/api/events', async (req, res) => {
+    const events = await Event.find();
+    res.json(events);
+});
+
+app.listen(5000, () => console.log("Server running on port 5000"));
